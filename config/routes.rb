2a0323296f62553413a94c2eab48df0b1c04937a
Matsu <matsu@shin.local>
2022-12-10 19:19:43 +0900

    Add default devise function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :stores, controllers: {
+    sessions:      'stores/sessions',
+    passwords:     'stores/passwords',
+    registrations: 'stores/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
