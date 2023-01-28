bdbd5ec5637de8d719a525cc3c09bef8495d2e8f
Matsu <matsu@shin.local>
2023-01-28 16:25:34 +0900

    Add /pages/term.html.erb, /pages/privacy.html.erb to /_footer.html.erb

---
+++ app/javascript/packs/nav.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.tab').on('click', function() {
+    $('.tab, .panel').removeClass('active');
+    $(this).addClass('active');
+    var index = $('.tab').index(this);
+    $('.panel').eq(index).addClass('active');
+  });
+});
