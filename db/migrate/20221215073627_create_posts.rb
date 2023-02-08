8b0087e1b215483662e5b4addf639e502a8212bd
Matsu <matsu@shin.local>
2023-02-08 12:39:46 +0900

    Add options to migration files

--- db/migrate/20221215073627_create_posts.rb
+++ db/migrate/20221215073627_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
-    create_table :posts do |t|
+    create_table :posts do |t|, options: 'ROW_FORMAT=DYNAMIC' do |t|
       t.string :product_img
       t.string :product_introduction
       t.integer :store_id
