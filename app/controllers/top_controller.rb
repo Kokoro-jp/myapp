5b3776242eaf4af7fc7082b4e3578c7df5d8bc7c
Matsu <matsu@shin.local>
2023-02-22 17:06:49 +0900

    Fix layouts

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,11 +2,14 @@ class TopController < ApplicationController
   before_action :set_q, only: %i[index]
   def index
     @posts = if store_signed_in?
-               Post.where(store_id: current_store.id).where.not(product_img: nil)
-                   .includes(:store).limit(5).order('created_at DESC')
+               Post.where(store_id: current_store.id).includes(:store).limit(5).order('created_at DESC')
              else
-               Post.all.where.not(product_img: nil).limit(5).order('created_at DESC')
+               Post.limit(5).order('created_at DESC')
              end
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+    end
   end
 
   def set_q
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :autheniticate_user, unless: :user_signed_in?
 
   def home
-    @posts = Post.all.where.not(product_img: nil).order('created_at DESC').limit(5)
+    @posts = Post.all.order('created_at DESC').limit(5)
   end
 
   def show
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to :root, notice: t(:withdrawal_successfully)
   end
